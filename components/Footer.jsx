c0856cf8c1329e86480cb5fe1caee35313c418ee
Okuku Itorobong <patrickokuku1@gmail.com>
2023-06-11 21:04:12 +0100

    footer & global style updates

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="h-[70px] flex_row items-center justify-center text-[20px] font-semibold">
+    <footer className="h-[100px] flex_row items-center justify-center text-[20px] font-semibold">
       <h2 className="mr-3">Powered by</h2>
       <Image
         className="rounded-full"
