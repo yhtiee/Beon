8a90dbe8f4ddc2f7d19a2e2f040688f90113e95a
Okuku Itorobong <patrickokuku1@gmail.com>
2023-06-12 12:01:52 +0100

    user sign-up page update

--- app/brand/sign-up/page.jsx
+++ app/brand/sign-up/page.jsx
@@ -1,31 +1,56 @@
 "use client";
-"use client";
 import { useState } from "react";
 import Image from "next/image";
 import { BsExclamationCircle } from "react-icons/bs";
+import { RiArrowDownSLine, RiCloseLine } from "react-icons/ri";
+import { niche } from "@/constants";
 
 const page = () => {
   const [page, setPage] = useState(1);
+  const [showNicheOps, setShowNicheOps] = useState(false);
+  const [selectedNicheOps, setSelectedNicheOps] = useState([]);
 
+  const [brand, setBrand] = useState({
+    name: "",
+    website: "",
+    selectedNiche: [],
+    whitePaper: "",
+    twitter: "",
+    instagram: "",
+  });
+
+  /* Handles the btn to previous page(1) */
   const handleBack = (e) => {
     e.preventDefault();
     setPage(1);
   };
 
+  /* Handles the btn to next page(2) */
   const handleNext = (e) => {
     e.preventDefault();
     setPage(2);
   };
 
+  /* Handles adding and removing Niche options */
+  const handleClickedOps = (e) => {
+    const option = e.currentTarget.attributes["tag"].value;
+    if (selectedNicheOps.includes(option)) {
+      const newOps = selectedNicheOps.filter((item) => item !== option);
+      setSelectedNicheOps(newOps);
+    } else setSelectedNicheOps([...selectedNicheOps, option]);
+
+    setShowNicheOps(false);
+  };
+
   return (
     <>
+      {/* Form Page 1 */}
       <section
         className={`${
           page == 2 ? "hidden" : "flex flex-row"
         }  items-center justify-center my-12 relative`}
       >
-        <div className="bg_gradient absolute bottom-0 w-[70vw] bg_gradient_animate z-10" />
-        <div className="sign_in_wrapper_gradient p-10 gap-10 w-[42vw] h-[520px]">
+        <div className="sign_in_wrapper_gradient p-10 gap-10 w-[45vw] h-[680px]">
           <div className="flex flex-row justify-between items-center mb-6">
             <p className="text-signIn font-semibold text-[16px]">
               Company Information
@@ -34,7 +59,9 @@ const page = () => {
               <span className="text-[#00ef8b]">Step {page}</span> of 2
             </p>
           </div>
-          <div className="flex_row justify-center items-center sign_in_gallery_gradient w-[66px] h-[66px] rounded-full ">
+
+          {/* Logo */}
+          <div className="flex_row justify-center items-center sign_in_gallery_gradient w-[66px] h-[66px] rounded-full">
             <Image
               src="/images/gallery-icon.png"
               alt="Gallery icon"
@@ -51,48 +78,104 @@ const page = () => {
           </div>
 
           <form className="flex_col mt-8">
+            {/* Project Name */}
             <label htmlFor="name" className="text-signIn">
               Name
             </label>
             <input
               className="input_text"
               name="name"
+              value={brand.name}
+              onChange={(e) => setBrand({ ...brand, name: e.target.value })}
               type="text"
-              placeholder="Flow_Project"
+              required
+              placeholder="flow project"
             />
+
+            {/* Website */}
             <label htmlFor="website" className="text-signIn mt-3">
               Website
             </label>
             <input
               className="input_text"
               name="website"
+              value={brand.website}
+              onChange={(e) => setBrand({ ...brand, website: e.target.value })}
               type="text"
-              placeholder="www.Flow_Project.xyz"
+              placeholder="https://flow-project.xyz"
             />
-            <label htmlFor="whitePaper" className="text-signIn mt-3">
+            <div className="relative">
+              <p className="text-signIn">Niche</p>
+              <div className="input_text px-1 flex_row items-center">
+                {selectedNicheOps.length > 0 ? (
+                  selectedNicheOps.map((option, i) => (
+                    <div
+                      key={i}
+                      className="bg-[#272727] border-[1px] border-[#00ef8b] p-1 rounded-sm mr-2 flex_row items-center"
+                    >
+                      <p>{option}</p>
+                      <div className="border-l-[1px] cursor-pointer ml-3 pl-1">
+                        <RiCloseLine tag={option} onClick={handleClickedOps} />
+                      </div>
+                    </div>
+                  ))
+                ) : (
+                  <p className="text-signIn">Select your niche</p>
+                )}
+                <div className="cursor-pointer h-[90%] flex_row items-center ml-auto">
+                  <RiArrowDownSLine
+                    onClick={() => setShowNicheOps(!showNicheOps)}
+                  />
+                </div>
+              </div>
+              {showNicheOps && (
+                <div className="rounded-[5px] w-[95%] h-[86px] bg-[#2F2F2F] border-[1px] border-[#00ef8b] px-[12px] p-[6px] absolute mt-1 overflow-y-scroll">
+                  {niche.map((item, i) => (
+                    <p
+                      className={`${
+                        selectedNicheOps.includes(item) && "text-[#00ef8b]"
+                      } cursor-pointer hover:text-[#00ef8b]`}
+                      key={i}
+                      tag={item}
+                      onClick={handleClickedOps}
+                    >
+                      {item}
+                    </p>
+                  ))}
+                </div>
+              )}
+            </div>
+            <label htmlFor="white-paper" className="text-signIn mt-3">
               Whitepaper
             </label>
             <input
               className="input_text"
-              name="whitePaper"
+              name="white-paper"
+              value={brand.whitePaper}
+              onChange={(e) =>
+                setBrand({ ...brand, whitePaper: e.target.value })
+              }
               type="text"
-              placeholder="http://Flow_Project.xyz/whitepaper"
+              placeholder="https://flow-project.xyz/whitepaper"
             />
-            <button onClick={handleNext} className="input_btn">
+            <button
+              onClick={handleNext}
+              className="input_btn bg-[#00ef8b] text-black font-semibold"
+            >
               Next
             </button>
           </form>
         </div>
       </section>
 
+      {/* Form Page 2 */}
       {page == 2 && (
         <section
           className={`${
             page == 1 ? "hidden" : "flex flex-row"
           } items-center justify-center my-12 relative`}
         >
-          <div className="bg_gradient absolute bottom-0 w-[70vw] bg_gradient_animate z-10" />
-          <div className="sign_in_wrapper_gradient p-10 gap-10 w-[42vw] h-[420px]">
+          <div className="sign_in_wrapper_gradient p-10 gap-10 w-[42vw] h-[450px]">
             <div className="flex flex-row justify-between items-center mb-6">
               <p className="text-signIn font-semibold text-[16px]">
                 Social Links
@@ -109,8 +192,12 @@ const page = () => {
               <input
                 className="input_text"
                 name="twitter"
+                value={brand.twitter}
+                onChange={(e) =>
+                  setBrand({ ...brand, twitter: e.target.value })
+                }
                 type="text"
-                placeholder="http://twitter.com/Flow_Project"
+                placeholder="http://twitter.com/flow-project"
               />
               <label htmlFor="instagram" className="text-signIn mt-3">
                 Instagram
@@ -118,10 +205,23 @@ const page = () => {
               <input
                 className="input_text"
                 name="instagram"
+                value={brand.instagram}
+                onChange={(e) =>
+                  setBrand({ ...brand, instagram: e.target.value })
+                }
                 type="text"
-                placeholder="https://linked.In/Flow_Project"
+                placeholder="https://www.linkedin.com/in/flow-project"
               />
-              <button className="input_btn">Proceed</button>
+              <button className="input_btn bg-[#00ef8b] text-black font-semibold">
+                Proceed
+              </button>
+              <button
+                onClick={handleBack}
+                className="flex_col justify-center items-center  input_btn bg-transparent text-white font-semibold"
+              >
+                Back
+                <div className="w-[24px] border-[1px] border-white" />
+              </button>
             </form>
           </div>
         </section>
